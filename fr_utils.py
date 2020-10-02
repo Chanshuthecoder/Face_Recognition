713a915c65f1c530961c03865b791fcef38e2a36
Chanshu Kumar <56483623+Chanshuthecoder@users.noreply.github.com>
2020-10-02 08:09:14 +0530

    Add files via upload

---
+++ fr_utils.py
@@ -0,0 +1,119 @@
+#### PART OF THIS CODE IS USING CODE FROM VICTOR SY WANG: https://github.com/iwantooxxoox/Keras-OpenFace/blob/master/utils.py ####
+
+import tensorflow as tf
+import numpy as np
+import os
+import cv2
+from numpy import genfromtxt
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+import h5py
+import matplotlib.pyplot as plt
+import pickle
+
+_FLOATX = 'float32'
+
+def variable(value, dtype=_FLOATX, name=None):
+    v = tf.Variable(np.asarray(value, dtype=dtype), name=name)
+    _get_session().run(v.initializer)
+    return v
+
+def shape(x):
+    return x.get_shape()
+
+def square(x):
+    return tf.square(x)
+
+def zeros(shape, dtype=_FLOATX, name=None):
+    return variable(np.zeros(shape), dtype, name)
+
+def concatenate(tensors, axis=-1):
+    if axis < 0:
+        axis = axis % len(tensors[0].get_shape())
+    return tf.concat(axis, tensors)
+
+def LRN2D(x):
+    return tf.nn.lrn(x, alpha=1e-4, beta=0.75)
+
+def conv2d_bn(x,
+              layer=None,
+              cv1_out=None,
+              cv1_filter=(1, 1),
+              cv1_strides=(1, 1),
+              cv2_out=None,
+              cv2_filter=(3, 3),
+              cv2_strides=(1, 1),
+              padding=None):
+    num = '' if cv2_out == None else '1'
+    tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, data_format='channels_first', name=layer+'_conv'+num)(x)
+    tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
+    tensor = Activation('relu')(tensor)
+    if padding == None:
+        return tensor
+    tensor = ZeroPadding2D(padding=padding, data_format='channels_first')(tensor)
+    if cv2_out == None:
+        return tensor
+    tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, data_format='channels_first', name=layer+'_conv'+'2')(tensor)
+    tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+'2')(tensor)
+    tensor = Activation('relu')(tensor)
+    return tensor
+
+WEIGHTS = [
+  'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3',
+  'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
+  'inception_3a_pool_conv', 'inception_3a_pool_bn',
+  'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
+  'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
+  'inception_3b_3x3_conv1', 'inception_3b_3x3_conv2', 'inception_3b_3x3_bn1', 'inception_3b_3x3_bn2',
+  'inception_3b_5x5_conv1', 'inception_3b_5x5_conv2', 'inception_3b_5x5_bn1', 'inception_3b_5x5_bn2',
+  'inception_3b_pool_conv', 'inception_3b_pool_bn',
+  'inception_3b_1x1_conv', 'inception_3b_1x1_bn',
+  'inception_3c_3x3_conv1', 'inception_3c_3x3_conv2', 'inception_3c_3x3_bn1', 'inception_3c_3x3_bn2',
+  'inception_3c_5x5_conv1', 'inception_3c_5x5_conv2', 'inception_3c_5x5_bn1', 'inception_3c_5x5_bn2',
+  'inception_4a_3x3_conv1', 'inception_4a_3x3_conv2', 'inception_4a_3x3_bn1', 'inception_4a_3x3_bn2',
+  'inception_4a_5x5_conv1', 'inception_4a_5x5_conv2', 'inception_4a_5x5_bn1', 'inception_4a_5x5_bn2',
+  'inception_4a_pool_conv', 'inception_4a_pool_bn',
+  'inception_4a_1x1_conv', 'inception_4a_1x1_bn',
+  'inception_4e_3x3_conv1', 'inception_4e_3x3_conv2', 'inception_4e_3x3_bn1', 'inception_4e_3x3_bn2',
+  'inception_4e_5x5_conv1', 'inception_4e_5x5_conv2', 'inception_4e_5x5_bn1', 'inception_4e_5x5_bn2',
+  'inception_5a_3x3_conv1', 'inception_5a_3x3_conv2', 'inception_5a_3x3_bn1', 'inception_5a_3x3_bn2',
+  'inception_5a_pool_conv', 'inception_5a_pool_bn',
+  'inception_5a_1x1_conv', 'inception_5a_1x1_bn',
+  'inception_5b_3x3_conv1', 'inception_5b_3x3_conv2', 'inception_5b_3x3_bn1', 'inception_5b_3x3_bn2',
+  'inception_5b_pool_conv', 'inception_5b_pool_bn',
+  'inception_5b_1x1_conv', 'inception_5b_1x1_bn',
+  'dense_layer'
+]
+
+
+def load_weights_from_FaceNet(FRmodel):
+    # Load weights from csv files (which was exported from Openface torch model)
+    weights = WEIGHTS
+    # open a file, where you stored the pickled data
+    file = open('facenet_weights.pkl', 'rb')
+    # dump information to that file
+    weights_dict = pickle.load(file)
+    # close the file
+    file.close()
+
+    # Set layer weights of the model
+    for name in weights:
+        if FRmodel.get_layer(name) != None:
+            FRmodel.get_layer(name).set_weights(weights_dict[name])
+        elif model.get_layer(name) != None:
+            model.get_layer(name).set_weights(weights_dict[name])
+
+
+def img_to_encoding(image_path, model, path=True):
+    if path == False:
+      img1 = image_path
+    else:
+      img1 = cv2.imread(image_path, 1)
+    img = img1[...,::-1]
+    img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+    x_train = np.array([img])
+    print(x_train.shape)
+    embedding = model.predict_on_batch(x_train)
+    return embedding
